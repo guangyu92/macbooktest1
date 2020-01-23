918f0f20cf76504cd4f9f6c58b300df13fb58859
guangyu92 <653820908@qq.com>
2020-01-24 00:48:59 +0800

    2020.1.24

---
+++ .vscode/AI/first keras.py
@@ -0,0 +1,12 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers,optimizers,datasets
+
+(x,y),(x_val,y_val)=datasets.mnist.load_data()
+x=2*tf.convert_to_tensor(x,dtype=tf.float32)/255.-1
+y=tf.convert_to_tensor(y,dtype=tf.int32)
+y=tf.one_hot(y,depth=10)
+print(x.shape,y.shape)
+train_dataset=tf.data.Dataset.from_tensor_slices((x,y))
+train_dataset=train_dataset.batch(512)
